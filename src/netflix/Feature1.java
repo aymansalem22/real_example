8cec557816779a8822fd15eadd0fde8b3d531872
aymansalem22 <s.aymansalem@gmail.com>
2023-06-09 01:55:20 +0200

    Update Feature #1: Group Similar Titles

--- src/netflix/Feature1.java
+++ src/netflix/Feature1.java
@@ -8,43 +8,44 @@ import java.util.Map;
 
 public class Feature1 {
 
-	public static List<List<String>> groupTitles(String[] strs){
-		if(strs.length==0) 
+	public static List<List<String>> groupTitles(String[] strs) {
+		if (strs.length == 0)
 			return new ArrayList<List<String>>();
-		
-		Map<String,List<String>> res=new HashMap<String,List<String>>();
-		
-		int[] count=new int[26];
-		for(String s:strs) {
+
+		Map<String, List<String>> res = new HashMap<String, List<String>>();
+
+		int[] count = new int[26];
+		for (String s : strs) {
 			Arrays.fill(count, 0);
-			for(char c:s.toCharArray()) {
-				int index=c-'a';
+			for (char c : s.toCharArray()) {
+				int index = c - 'a';
 				count[index]++;
 			}
-			
-			//number of appearance the letter 
-			StringBuilder delimStr=new StringBuilder("");
-			for(int i=0;i<26;i++) {
+
+			// number of appearance the letter
+			StringBuilder delimStr = new StringBuilder("");
+			for (int i = 0; i < 26; i++) {
 				delimStr.append('#');
 				delimStr.append(count[i]);
 			}
-			
-			String key=delimStr.toString();
-			if(!res.containsKey(key))
-				res.put(key,new ArrayList<String>());
-			
+
+			String key = delimStr.toString();
+			if (!res.containsKey(key))
+				res.put(key, new ArrayList<String>());
+
 			res.get(key).add(s);
 		}
-		
+
 		return new ArrayList<List<String>>(res.values());
-		
+
 	}
+
 	public static void main(String[] args) {
-		String titles[]= {"duel","dule","speed","spede","deul","cars"};
-		List<List<String>> gt=groupTitles(titles);
-		String query="spede";
-		for(List<String> g:gt) {
-			if(g.contains(query))
+		String titles[] = { "duel", "dule", "speed", "spede", "sepde", "deul", "cars" };
+		List<List<String>> gt = groupTitles(titles);
+		String query = "spede";
+		for (List<String> g : gt) {
+			if (g.contains(query))
 				System.out.println(g);
 		}
 
