8c25bff8bc371a39a5cb34aee075931a953b63ea
aymansalem22 <s.aymansalem@gmail.com>
2023-06-18 13:48:08 +0200

    Feature #4: Popularity Analysis

---
+++ src/netflix/Feature4.java
@@ -0,0 +1,30 @@
+package netflix;
+
+public class Feature4 {
+
+	public static boolean identifyTitles(int[] scores) {
+		boolean increasing = true;
+		boolean decreasing = true;
+
+		for (int i = 0; i < scores.length - 1; i++) {
+			if (scores[i] > scores[i + 1])
+				increasing = false;
+			if (scores[i] < scores[i + 1])
+				decreasing = false;
+		}
+		return (increasing || decreasing);
+	}
+
+	public static void main(String[] args) {
+
+		int[][] movie_ratings = { { 1, 2, 2, 3 }, { 4, 5, 6, 3, 4 }, { 8, 8, 7, 6, 5, 4, 4, 1 },
+				{ 22, 33, 12, 12, 10 } };
+		for (int[] movie_rating : movie_ratings) {
+			if (identifyTitles(movie_rating))
+				System.out.println("Title Identified and Separated");
+			else
+				System.out.println("Title Score Fluctuating");
+		}
+
+	}
+}
