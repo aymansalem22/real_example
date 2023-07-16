eeca0e49bbf77b915877dc369ae200803617104b
aymansalem22 <s.aymansalem@gmail.com>
2023-07-16 15:31:08 +0200

    update Feature #2: Fetch Top Movies

--- src/netflix/LinkedList.java
+++ src/netflix/LinkedList.java
@@ -1,173 +1,175 @@
 package netflix;
 
-import java.util.ArrayList;
-import java.util.Random;
-
+import java.util.*;
+//using for Class Feature2
 class LinkedListNode {
-	public int key;
-	public int data;
-	public LinkedListNode next;
-	public LinkedListNode arbitraryPointer;
+    public int key;
+    public int data;
+    public LinkedListNode next;
+    public LinkedListNode arbitraryPointer;
 
-	public LinkedListNode(int data) {
-		this.data = data;
-		this.next = null;
-	}
+    public LinkedListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
 
-	public LinkedListNode(int key, int data) {
-		this.key = key;
-		this.data = data;
-		this.next = null;
-	}
+    public LinkedListNode(int key, int data) {
+        this.key = key;
+        this.data = data;
+        this.next = null;
+    }
 
-	public LinkedListNode(int data, LinkedListNode next) {
-		this.data = data;
-		this.next = next;
-	}
+    public LinkedListNode(int data, LinkedListNode next) {
+        this.data = data;
+        this.next = next;
+    }
 
-	public LinkedListNode(int data, LinkedListNode next, LinkedListNode arbitraPointer) {
-		this.data = data;
-		this.next = next;
-		this.arbitraryPointer = arbitraPointer;
-	}
+    public LinkedListNode(int data, LinkedListNode next, LinkedListNode arbitraryPointer) {
+        this.data = data;
+        this.next = next;
+        this.arbitraryPointer = arbitraryPointer;
+    }
 }
 
-class LinkedList {
-	public static LinkedListNode insertAtHead(LinkedListNode head, int data) {
-		LinkedListNode newNode = new LinkedListNode(data);
-		newNode.next = head;
-		return newNode;
-	}
 
-	public static LinkedListNode insertAtTail(LinkedListNode head, int data) {
-		LinkedListNode newNode = new LinkedListNode(data);
-		if (head == null) {
-			return newNode;
-		}
-		LinkedListNode temp = head;
-		while (temp.next != null) {
-			temp = temp.next;
-		}
 
-		temp.next = newNode;
-		return head;
-	}
+class LinkedList { 
 
-	public static LinkedListNode insertAtTail(LinkedListNode head, LinkedListNode node) {
-		if (head == null) {
-			return node;
-		}
+    public static LinkedListNode insertAtHead(LinkedListNode head, int data) {
+        LinkedListNode newNode = new LinkedListNode(data);
+        newNode.next = head;
+        return newNode;
+    }
 
-		LinkedListNode temp = head;
-		while (temp.next != null) {
-			temp = temp.next;
-		}
-		temp.next = node;
-		return head;
-	}
+    public static LinkedListNode insertAtTail(LinkedListNode head, int data) {
+        LinkedListNode newNode = new LinkedListNode(data);
+        if (head == null) {
+            return newNode;
+        }
+        LinkedListNode temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        return head;
+    }
 
-	public static LinkedListNode createLinkedList(ArrayList<Integer> lst) {
-		LinkedListNode head = null;
-		LinkedListNode tail = null;
-		for (Integer x : lst) {
-			LinkedListNode newNode = new LinkedListNode(x);
-			if (head == null) {
-				head = newNode;
-			} else {
-				tail.next = newNode;
-			}
-			tail = newNode;
-		}
-		return head;
-	}
+    public static LinkedListNode insertAtTail(LinkedListNode head, LinkedListNode node)
+    {
+        if (head == null) {
+            return node;
+        }
+        LinkedListNode temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = node;
+        return head;
+    }
 
-	public static LinkedListNode createLinkedList(int[] arr) {
-		LinkedListNode head = null;
-		LinkedListNode tail = null;
-		for (int i = 0; i < arr.length; ++i) {
-			LinkedListNode newNode = new LinkedListNode(arr[i]);
-			if (head == null) {
-				head = newNode;
-			} else {
-				tail.next = newNode;
-			}
-			tail = newNode;
-		}
-		return head;
-	}
+    public static LinkedListNode createLinkedList(ArrayList<Integer> lst) {
+        LinkedListNode head = null;
+        LinkedListNode tail = null;
+        for (Integer x : lst) {
+            LinkedListNode newNode = new LinkedListNode(x);
+            if (head == null) {
+                head = newNode;
+            } else {
+                tail.next = newNode;
+            }
+            tail = newNode;
+        }
+        return head;
+    }
 
-	public static LinkedListNode createRandomList(int length) {
-		LinkedListNode listHead = null;
-		Random generator = new Random();
-		for (int i = 0; i < length; ++i) {
-			listHead = insertAtHead(listHead, generator.nextInt(100));
-		}
-		return listHead;
-	}
+    public static LinkedListNode createLinkedList(int[] arr) {
+        LinkedListNode head = null;
+        LinkedListNode tail = null;
+        for (int i = 0; i < arr.length; ++i) {
+            LinkedListNode newNode = new LinkedListNode(arr[i]);
+            if (head == null) {
+                head = newNode;
+            } else {
+                tail.next = newNode;
+            }
+            tail = newNode;
+        }
+        return head;
+    }
 
-	public static ArrayList<Integer> toList(LinkedListNode head) {
-		ArrayList<Integer> lst = new ArrayList<Integer>();
-		LinkedListNode temp = head;
-		while (temp != null) {
-			lst.add(temp.data);
-			temp = temp.next;
-		}
-		return lst;
-	}
+    public static LinkedListNode createRandomList(int length) {
+        LinkedListNode listHead = null;
+        Random generator = new Random();
+        for(int i = 0; i < length; ++i) {
+            listHead = insertAtHead(listHead, generator.nextInt(100));
+        }
+        return listHead;
+    }
 
-	public static void display(LinkedListNode head) {
-		LinkedListNode temp = head;
-		while (temp != null) {
-			System.out.printf("%d", temp.data);
-			temp = temp.next;
-			if (temp != null) {
-				System.out.printf(", ");
-			}
-		}
-		System.out.println();
-	}
+    public static ArrayList<Integer> toList(LinkedListNode head) {
+        ArrayList<Integer> lst = new ArrayList<Integer>();
+        LinkedListNode temp = head;
+        while (temp != null) {
+            lst.add(temp.data);
+            temp = temp.next;
+        }
+        return lst;
+    }
 
-	public static LinkedListNode mergeAlternating(LinkedListNode list1, LinkedListNode list2) {
-		if (list1 == null) {
-			return list2;
-		}
+    public static void display(LinkedListNode head) {
+        LinkedListNode temp = head;
+        while (temp != null) {
+            System.out.printf("%d", temp.data);
+            temp = temp.next;
+            if (temp != null) {
+              System.out.printf(", ");
+            }
+        }
+        System.out.println();
+    }  
+    
 
-		if (list2 == null) {
-			return list1;
-		}
+    public static LinkedListNode mergeAlternating(LinkedListNode list1, LinkedListNode list2) {
+      if (list1 == null) {
+        return list2;
+      }
 
-		LinkedListNode head = list1;
+      if (list2 == null) {
+        return list1;
+      }
 
-		while (list1.next != null && list2 != null) {
-			LinkedListNode temp = list2;
-			list2 = list2.next;
+      LinkedListNode head = list1;
 
-			temp.next = list1.next;
-			list1.next = temp;
-			list1 = temp.next;
-		}
+      while (list1.next != null && list2 != null) {
+        LinkedListNode temp = list2;
+        list2 = list2.next;
 
-		if (list1.next == null) {
-			list1.next = list2;
-		}
+        temp.next = list1.next;
+        list1.next = temp;
+        list1 = temp.next;
+      }
 
-		return head;
-	}
+      if (list1.next == null) {
+        list1.next = list2;
+      }
 
-	static boolean isEqual(LinkedListNode list1, LinkedListNode list2) {
-		if (list1 == list2) {
-			return true;
-		}
+      return head;
+    }
 
-		while (list1 != null && list2 != null) {
-			if (list1.data != list2.data) {
-				return false;
-			}
+    static boolean isEqual(LinkedListNode list1, LinkedListNode list2) {
+        if (list1 == list2) {
+            return true;
+        }
 
-			list1 = list1.next;
-			list2 = list2.next;
-		}
+        while (list1 != null && list2 != null) {
+            if (list1.data != list2.data) {
+                return false;
+            }
 
-		return (list1 == list2);
-	}
+            list1 = list1.next;
+            list2 = list2.next;
+        }
+
+        return (list1 == list2);
+    }
 }
